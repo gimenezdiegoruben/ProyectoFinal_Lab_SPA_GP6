d5e110f8f9871195c8f7c079adf2c0d3a26d371e
gimenezdiegoruben <gimenezdiegoruben@gmail.com>
2025-11-02 19:36:19 -0300

    Update spafinal_gp6.sql

--- spafinal_gp6.sql
+++ spafinal_gp6.sql
@@ -20,6 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `spafinal_gp6`
 --
+CREATE DATABASE IF NOT EXISTS `spafinal_gp6` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `spafinal_gp6`;
 
 -- --------------------------------------------------------
 
